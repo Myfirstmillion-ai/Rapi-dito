4d326ff26dd9b7ed56fbaf32fbe1631883ea0156
Myfirstmillion-ai <lilnazx1115@gmail.com>
2025-12-10 19:15:18 -0400

    Crear index.ts

---
+++ Frontend/src/components/layout/index.ts
@@ -0,0 +1,6 @@
+export { AppShell } from './AppShell';
+export { BottomSheet } from './BottomSheet';
+export { FloatingActionBar } from './FloatingActionBar';
+export { SafeAreaView } from './SafeAreaView';
+export { StatusBar } from './StatusBar';
+export * from './types';
