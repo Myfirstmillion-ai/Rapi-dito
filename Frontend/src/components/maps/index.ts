6a7604655f051f0a7c9262058804cc4b2079adf7
Myfirstmillion-ai <lilnazx1115@gmail.com>
2025-12-10 19:11:26 -0400

    Crear index.ts

---
+++ Frontend/src/components/maps/index.ts
@@ -0,0 +1,6 @@
+export { LuxuryMap } from './LuxuryMap';
+export { MapControls } from './MapControls';
+export { MapMarker } from './MapMarker';
+export { RouteLayer } from './RouteLayer';
+export * from './mapStyles';
+export * from './types';
